9b3936bd70765b494c6ad29dca9b3c217ee81177
Kingston Yuen <kyuen@cps.edu>
2026-05-06 11:40:00 -0500

    progress

--- a10.py
+++ a10.py
@@ -114,7 +114,8 @@ def get_birth_date(name: str) -> str:
         birth date of the given person
     """
     infobox_text = clean_text(get_first_infobox_text(get_page_html(name)))
-    pattern = r"(?:Born\D*)(?P<birth>\d{4}-\d{2}-\d{2})"
+    print(infobox_text)
+    pattern = r"(?:Born|Date of birth.*)(?P<birth>\d{4}-\d{2}-\d{2})"
     error_text = (
         "Page infobox has no birth information (at least none in xxxx-xx-xx format)"
     )
@@ -150,6 +151,11 @@ def polar_radius(matches: List[str]) -> List[str]:
         polar radius of planet
     """
     return [get_polar_radius(matches[0])]
+
+def basketball_team(nba_team: str) -> str:
+    infobox_text = clean_text(get_first_infobox_text(get_page_html(nba_team)))
+    print(infobox_text)
+    pattern = r"(?:"
 
 
 # dummy argument is ignored and doesn't matter
